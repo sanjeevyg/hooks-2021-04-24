b381f17a8442430493bc1dfd584148ada28ef965
sanjeevyg <yogi.sjv@gmail.com>
2021-09-22 11:21:43 -0400

    quote url updated

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import taylorState from './taylorState.js'
 
 
 export default function App() {
-    const [quote, img, upCount, downCount, count] = taylorState(0)
-    console.log({img})
+    const [upCount, downCount, count, quote] = taylorState(1)
+    // console.log({img})
   
     return (
         <div className="App">
             <h4>Count: {count}</h4>
             <button onClick={upCount}>Increase Count</button>
             <button onClick={downCount}>Decrease Count</button>
-            <h2>{quote}</h2>
-            <img src={img.url} alt="taylor"/>
+            {/* <h2>{quote}</h2> */}
+            <img src={quote} alt="taylor" width="900" height="400"/>
         </div>
     )
 }
--- src/taylorState.js
+++ src/taylorState.js
@@ -2,34 +2,38 @@ import {useState, useEffect} from 'react'
 
 
 
-const quoteURL = "https://api.taylor.rest"
-const imgURL = "https://api.taylor.rest/image"
+// const quoteURL = "https://api.taylor.rest"
+// const imgURL = "https://api.taylor.rest/image"
 
-export default function TaylorState() {
-    const [count, setCount] = useState(0)
-    const [img, setImage] = useState("")
+export default function TaylorState(initialCount) {
+    const [count, setCount] = useState(initialCount)
+    // const [img, setImage] = useState("")
     const [quote, setQuote] = useState("")
-
+    
     const upCount = () => setCount(count + 1)
     const downCount = () => setCount(count - 1)
-
+    
+    const imgURL = `https://web-series-quotes.herokuapp.com/generate/gameofthrones/${count}/image?src=gameofthrones&size=150&color=lightyellow`
+    // const imgURL = "https://web-series-quotes.herokuapp.com/gameofthrones"
 
     const fetchQuote = () => {
-        fetch(quoteURL)
-         .then(response => response.json())
-         .then(quote => setQuote(quote.quote))
+        setQuote(imgURL)
     }
 
-    const fetchURL = () => {
-        fetch(imgURL)
-         .then(response => response.json())
-         .then(image => console.log(image.url))
-    }
+    console.log(quote)
+
+    
+
+    // const fetchURL = () => {
+    //     fetch(imgURL)
+    //      .then(response => response.json())
+    //      .then(image => setImage(image))
+    // }
      
     useEffect(() => {
         fetchQuote()
-        fetchURL()
+        // fetchURL()
     }, [count])
 
-    return [quote, upCount, downCount, img, count]
+    return [ upCount, downCount, count, quote ]
 }
